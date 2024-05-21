929f1cc6e95ede0c4161bd58a46bc36b02249c92
radek.stary <radek.stary@gmail.com>
2024-05-22 00:49:02 +0200

    ellipsis pipe

---
+++ src/app/pipes/ellipsis.pipe.ts
@@ -0,0 +1,16 @@
+import { SlicePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'ellipsis',
+  standalone: true,
+})
+export class EllipsisPipe implements PipeTransform {
+  transform(value: string, maxLength: number = 30): string {
+    if (value.length <= maxLength) {
+      return value;
+    } else {
+      return value.slice(0, maxLength) + '...';
+    }
+  }
+}
